8c0a71b4e0fe478b0cb06ab88605a0c4343c1be9
HENRIQUE RODRIGUES VIEIRA <hvieira@br.ibm.com>
2018-06-06 13:55:39 -0300

    criação do login e integração com a api graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { FacebookService, InitParams } from "ngx-facebook/dist/esm";
+import { FacebookService, InitParams, LoginResponse, LoginOptions } from "ngx-facebook/dist/esm";
+import { Pagina } from "src/app/pagina";
 
 
 @Component({
@@ -17,6 +18,9 @@ export class AppComponent {
   user: Observable<firebase.User>;
   email: string;
   senha: string;
+  id: string;
+
+  paginas: Pagina[] = [];
 
   constructor(public afAuth: AngularFireAuth, private http: Http, private fb: FacebookService) {
     this.user = this.afAuth.authState;
@@ -31,23 +35,77 @@ export class AppComponent {
   }
 
   loginFacebook() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+   // this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+   this.fb.login()
+      .then((res: LoginResponse) => {
+        console.log('Logged in', res);
+      })
+      .catch(this.handleError);
+  }
+
+  loginWithOptions() {
+
+    const loginOptions: LoginOptions = {
+      enable_profile_selector: true,
+      return_scopes: true,
+      scope: 'public_profile, user_likes'
+    };
+
+    this.fb.login(loginOptions)
+      .then((res: LoginResponse) => {
+        console.log('Logged in', res);
+        this.id = this.fb.getAuthResponse().userID;
+      })
+      .catch(this.handleError);
+  }
+
+  mostraToken(){
+    console.log(this.validaToken());
+  }
+
+  mostraUsuario(){
+    console.log(this.fb.getAuthResponse().userID);
+    this.id = this.fb.getAuthResponse().userID;
+  }
+
+  retornaPagina(){
+     this.fb.api('444224355588448')
+      .then((res: any) => {
+        console.log('Got the users profile', res);
+      })
+      .catch(this.handleError);
   }
 
   validaToken() {
-    this.fb.getLoginStatus();
+    if(this.fb.getAuthResponse()){
+    return true;
+    }
+    else
+    return false;
   }
 
   getDados() {
-    this.validaToken();
-    this.fb.api('/me/likes', 'get', function (response) {
-      console.log("dados: " + response);
-      if (!response || response.error) {
-        alert('Error occured');
-      } else {
-        alert('Post ID: ' + response.id);
-      }
-    });
+    this.fb.api('me/likes')
+      .then((res: any) => {
+        this.paginas = res.data;   })
+      .catch(this.handleError);
+  }
+
+  imprimeDados(){
+    console.log(this.paginas);
+  }
+
+  getFoto() {
+    this.fb.api('/me/picture')
+      .then((res: any) => {
+        console.log('Got the users profile', res);
+      })
+      .catch(this.handleError);
+
+  }
+
+  private handleError(error) {
+    console.error('Error processing action', error);
   }
 
   loginEmail() {
@@ -57,6 +115,6 @@ export class AppComponent {
   }
 
   sair() {
-    this.afAuth.auth.signOut();
+    this.fb.logout();
   }
 }
---
+++ src/app/pagina.ts
@@ -0,0 +1,5 @@
+export class Pagina {
+    name: string;
+    id: string;
+    created_time: string;
+}
